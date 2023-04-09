c2813b101595b626505174322fbfdface6a32f45
volodymyr_koshuk <vovakos2001@gmail.com>
2023-04-09 16:44:10 +0300

    Added attach IAM Role to EC2 for Session Manager Connection. Basic configuration to module vpc.

--- project.tf
+++ project.tf
@@ -1,17 +1,18 @@
 provider "aws" {}
 
 module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "4.3.0"
+  source   = "terraform-aws-modules/ec2-instance/aws"
+  version  = "4.3.0"
   for_each = toset(["first", "second", "third"])
 
   name = "LinuxAWS Server"
 
-  ami           = "ami-0cbfcdb45dcced1ca"
-  instance_type = "t3.micro"
-  key_name      = "vova-key-linuxaws-prod-stokholm"
+  ami                         = "ami-0cbfcdb45dcced1ca"
+  instance_type               = "t3.micro"
+  key_name                    = "vova-key-linuxaws-prod-stokholm"
   associate_public_ip_address = false
-  putin_khuylo = true
+  iam_instance_profile        = "AmazonSSMRoleForInstancesQuickSetup"
+  putin_khuylo                = true
 
   root_block_device = [
     {
@@ -22,17 +23,43 @@ module "ec2-instance" {
   ]
 
   tags = {
-    Name = "Linux AWS Server #${each.key}"
+    Name      = "Linux AWS Server #${each.key}"
     Terraform = true
   }
 }
 
-
+/*
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.1"
   
   name = "my_SSU_VPC"
+  cidr = "10.2.0.0/16"
 
-  azs = [""]
+  azs             = ["eu-north-1a", "eu-north-1c"]
+  public_subnets  = ["10.2.11.0/24", "10.2.21.0/24"]
+  private_subnets = ["10.2.12.0/24", "10.2.22.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  tags = {
+    Name      = "My_SSU_VPC"
+    Terraform = true
+    education = true
+    course    = "devops4sysadmins"
+  }
+
+  public_subnet_tags  = {
+    Name = "My_public_subnet_SSU"
+    Terraform = true
+    Role = "public"
+  }
+  private_subnet_tags = {
+    Name = "My_private_subnet_SSU"
+    Terraform = true
+    Role = "private"
+  }
 }
+*/
