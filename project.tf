701f3882a4f544d03005bb6c960e9fb71b114ae0
volodymyr_koshuk <vovakos2001@gmail.com>
2023-04-09 19:03:27 +0300

    Added resources for create security groups in VPC SSU for Public and Private servers

--- project.tf
+++ project.tf
@@ -3,16 +3,16 @@ provider "aws" {}
 module "ec2-instance" {
   source   = "terraform-aws-modules/ec2-instance/aws"
   version  = "4.3.0"
-  for_each = toset(["first", "second", "third"])
+  for_each = toset(["first", "second"]) # Meta-argument for create 2 servers
 
-  name = "LinuxAWS Server"
+  name     = "Private LinuxAWS Server"
 
   ami                         = "ami-0cbfcdb45dcced1ca"
   instance_type               = "t3.micro"
   key_name                    = "vova-key-linuxaws-prod-stokholm"
   associate_public_ip_address = false
   iam_instance_profile        = "AmazonSSMRoleForInstancesQuickSetup"
-  vpc_security_group_ids = [""]
+  vpc_security_group_ids = [aws_security_group.private.id]
   putin_khuylo                = true
 
   root_block_device = [
@@ -24,8 +24,10 @@ module "ec2-instance" {
   ]
 
   tags = {
-    Name      = "Linux AWS Server #${each.key}"
+    Name      = "Private LinuxAWS Server #${each.key}"
     Terraform = true
+    education = true
+    course    = "devops4sysadmins"
   }
 }
 
@@ -37,10 +39,12 @@ module "vpc" {
   name = "my_SSU_VPC"
   cidr = "10.2.0.0/16"
 
+# Config Aviability Zone and Subnet
   azs             = ["eu-north-1a", "eu-north-1c"]
   public_subnets  = ["10.2.11.0/24", "10.2.21.0/24"]
   private_subnets = ["10.2.12.0/24", "10.2.22.0/24"]
 
+# Config NAT Gateway
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
@@ -59,13 +63,10 @@ module "vpc" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = "0.0.0.0/0"
+    cidr_blocks      = "10.2.0.0/16"
   }]
 
-  default_security_group_tags = {
-    Name = "Default Security Group SSU"
-  }
-
+# All Tags in VPC
   tags = {
     Name      = "My_SSU_VPC"
     Terraform = true
@@ -82,5 +83,75 @@ module "vpc" {
     Name = "My_private_subnet_SSU"
     Terraform = true
     Role = "private"
+  }
+
+  default_security_group_tags = {
+    Name = "Default Security Group SSU"
+  }
+}
+
+
+# Create Public SG for VPC of SSU
+resource "aws_security_group" "public" {
+  vpc_id = module.vpc.vpc_id
+  name   = "Security Group for Server of Public Subnet"
+
+# Rule for SSH conection
+  ingress {
+    description      = "SSH Port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+# Rule for HTTP conection
+  ingress {
+    description      = "HTTP Port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+# Rule for HTTPS conection
+  ingress {
+    description      = "HTTPS Port"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+# Rule for egress to all intenet
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
+# Create Private SG for VPC of SSU
+resource "aws_security_group" "private" {
+  vpc_id = module.vpc.vpc_id
+  name   = "Security Group for Server of Private Subnet"
+
+# Rule for SSH conection
+  ingress {
+    description      = "SSH Port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.2.0.0/16"]
+  }
+
+# Rule for egress to all intenet
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
