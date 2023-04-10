eceff7b32f73ba9c28154042378e24fc031ef335
volodymyr_koshuk <vovakos2001@gmail.com>
2023-04-11 01:33:18 +0300

    Rename files: project.tf --> main.tf, backup.tf --> version.tf. Remove provider in version.tf file. And added state lock for tfstate file

---
+++ version.tf
@@ -0,0 +1,11 @@
+provider "aws" {}
+
+terraform {
+  backend "s3" {
+    bucket = "koshuk-backup-terrafrom"
+    key    = "lab_project/terraform.tfstate"
+    region = "eu-north-1"
+    dynamodb_table = "terraform_state_aws_eu_north_1"
+  }
+}
+ 
